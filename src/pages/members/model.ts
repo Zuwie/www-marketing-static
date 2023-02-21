649ee8c0f5648ff4126439fffd1f2859d2dcbf43
rafaelseifert <rs.frontend@gmail.com>
2023-02-21 21:39:04 +0100

    stop being annoying build cmd

--- src/pages/members/model.ts
+++ src/pages/members/model.ts
@@ -1,11 +1,9 @@
-import { StrapiImage } from "@/src/models";
-
 export interface IMember {
   id: number
   attributes: {
     name: string;
     bio: string;
     role: string;
-    profilePicture: StrapiImage
+    profilePicture: string;
   }
 }
