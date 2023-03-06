01dac7309c9d1bd8acd5b4863ce6a3fa70e42f3a
rafaelseifert <rs.frontend@gmail.com>
2023-03-06 14:15:06 +0100

    create reuseable modal

---
+++ src/components/MyModal.tsx
@@ -0,0 +1,81 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import ReactMarkdown from "react-markdown";
+
+export default function MyModal({button, title, details}) {
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  return (
+    <>
+      <div>
+        <button
+          type="button"
+          onClick={openModal}
+          className="focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo focus-visible:ring-opacity-75"
+        >
+          {button}
+        </button>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-4xl font-bold leading-6 text-gray-900"
+                  >
+                    {title}
+                  </Dialog.Title>
+                  <div className="mt-6 text-base">
+                    <ReactMarkdown>{details}</ReactMarkdown>
+                  </div>
+
+                  <div className="mt-8">
+                    <button
+                      type="button"
+                      className="rounded-md bg-indigo-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                      onClick={closeModal}
+                    >
+                      Schließen
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
