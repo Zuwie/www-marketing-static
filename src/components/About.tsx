adec4c048b4ef2bca4cd2cfb8b917f95f33e7403
rafaelseifert <rs.frontend@gmail.com>
2023-03-13 18:06:00 +0100

    parse markdown for intros

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import { ServerIcon } from '@heroicons/react/20/solid'
 import Image from "next/image";
+import ReactMarkdown from "react-markdown";
 
 export default function About({image, subTitle, title, intro, list}) {
   return (
@@ -10,7 +11,9 @@ export default function About({image, subTitle, title, intro, list}) {
             <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
               <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">{subTitle}</h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">{intro}</p>
+              <div className="mt-6 text-lg leading-8 text-gray-600">
+                <ReactMarkdown>{intro}</ReactMarkdown>
+              </div>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {list.map((listItem) => (
                   <div key={listItem.title} className="relative pl-9">
--- src/components/AboutSecond.tsx
+++ src/components/AboutSecond.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { ServerIcon } from "@heroicons/react/24/outline";
+import ReactMarkdown from "react-markdown";
 
 export default function AboutSecond({image, subTitle, title, intro, list}) {
   return (
@@ -10,7 +11,9 @@ export default function AboutSecond({image, subTitle, title, intro, list}) {
             <div className="lg:max-w-lg">
               <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">{subTitle}</h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">{intro}</p>
+              <div className="mt-6 text-lg leading-8 text-gray-600">
+                <ReactMarkdown>{intro}</ReactMarkdown>
+              </div>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {list.map((listItem) => (
                   <div key={listItem.title} className="relative pl-9">
