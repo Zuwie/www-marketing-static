310ee765d4a29a9021af2287f0e1abc03c66178e
rafaelseifert <rs.frontend@gmail.com>
2023-03-14 16:26:26 +0100

    remove redundant log

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -14,7 +14,6 @@ type Props = {
   };
 };
 export default function PostList({posts, pagination}: Props) {
-  console.log(posts)
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
