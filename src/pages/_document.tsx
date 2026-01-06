684290daf65caa29ffa02ea80e5cb15245b2b3e7
zuwie <rs.webdev@icloud.com>
2026-01-06 15:59:28 +0100

    chore: set prefered url for google indexing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,16 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import {Head, Html, Main, NextScript} from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="de">
       <Head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async/>
-        <link rel="icon" type="image/png" href="favicon.png" sizes="56x56"/>
+        <link rel="canonical" href="https://wuerfelwappler.at/" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async />
+        <link rel="icon" type="image/png" href="favicon.png" sizes="56x56" />
       </Head>
       <body>
-      <Main/>
-      <NextScript/>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
